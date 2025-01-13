fcc4e313bb632d2af2489d3c8cad2c6c1227947b
JorgRibeiro <yorkis.gamer32@gmail.com>
2025-01-12 23:05:57 -0300

    Primeira tentativa - desafio 1

--- C/Desafio 1/numeros.c
+++ C/Desafio 1/numeros.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
 
-int* tranformar(int numero);
+int *tranformar(int numero);
 
+int main(void)
+{
 
-int main(void){
+    int numero;
+    scanf("%d", &numero);
 
-int a = 10;
+    tranformar(numero);
 
-
-return 0;
+    return 0;
 }
 
-int* tranformar(int numero){
+int *tranformar(int numero)
+{
 
+    int vetor[0];
+    char string[30];
+    int i;
+
+    sprintf(string, "%d", numero);
+     printf("%s", string);
+  
+    if (numero == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        for (i = 0; string[i] != '\n'; i++)
+        {
+            vetor[i] = string[i];
+        }
+    }
+    for (int j = 0; j < i; j++)
+    {
+        printf("[%d]", vetor[j]);
+    }
+
+    return vetor;
 }
